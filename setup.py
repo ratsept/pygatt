4b9a092ec8419a03218cd6fa97d1b307b1ef43cc
David Gelvin <david.gelvin@gmail.com>
2014-08-14 13:07:29 -0600

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name='pygatt',
+    version='0.10',
+    description='Python Bluetooth 4.0 bluez gatttool wrapper',
+    author='David Gelvin',
+    author_email='david.gelvin@gmail.com',
+    url='https://github.com/Sonopro/pygatt',
+    packages=['pygatt'],
+    install_requires= [
+        'sh>=1.09'
+    ]
+)
